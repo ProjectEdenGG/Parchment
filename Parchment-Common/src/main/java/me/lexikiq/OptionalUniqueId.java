26f9a018bb0e9c392f4531aa8098718eb7dc3ddb
lexikiq <noellekiq@gmail.com>
2021-05-02 14:31:43 -0400

    Create Parchment-Common
    eden-api was causing issues so I'm moving Parchment's common interfaces and utilities here

---
+++ Parchment-Common/src/main/java/me/lexikiq/OptionalUniqueId.java
@@ -0,0 +1,17 @@
+package me.lexikiq;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+/**
+ * Represents an object that may have a {@link UUID}
+ */
+public interface OptionalUniqueId {
+	/**
+	 * Returns a unique and persistent id for this object which may be null
+	 *
+	 * @return unique id or null
+	 */
+	@Nullable UUID getUniqueId(); // named getUniqueId to maintain compatibility with Bukkit
+}
