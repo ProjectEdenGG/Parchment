96b82fd6af7d41c71e1a734a09898b8f5e367950
Blast-MC <cjblanton2@gmail.com>
2025-12-12 14:34:41 -0500

    add PreHangingEntityBreakEvent

---
+++ parchment-api/src/main/java/gg/projecteden/parchment/event/entity/PreHangingEntityBreakEvent.java
@@ -0,0 +1,34 @@
+package gg.projecteden.parchment.event.entity;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class PreHangingEntityBreakEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private boolean cancelled = true; // default to true, nexus will set to false
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = true;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return handlers;
+    }
+
+    @NotNull
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+}
